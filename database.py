063471b0fc81744c6c66ccac87c1729dd876e4c5
Kagami <shimura.kyohei7617@gmail.com>
2024-03-26 20:58:33 +0900

    feat: get db

--- database.py
+++ database.py
@@ -6,3 +6,11 @@ DATABASE_URL = "postgresql://postgres:postgres@0.0.0.0:25432/admin"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
