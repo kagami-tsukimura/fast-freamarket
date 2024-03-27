150d2f5d0622602b4120c3cb003a5f0d9b84983a
Kagami <shimura.kyohei7617@gmail.com>
2024-03-27 22:06:42 +0900

    feat: password hash

--- cruds/auth.py
+++ cruds/auth.py
@@ -1,3 +1,7 @@
+import base64
+import hashlib
+import os
+
 from sqlalchemy.orm import Session
 
 from models import User
@@ -5,7 +9,20 @@ from schemas import UserCreate
 
 
 def create_user(db: Session, user_create: UserCreate):
-    new_user = User(**user_create.model_dump())
+    salt = base64.b64encode(os.urandom(32))
+    # パスワードのハッシュ化
+
+    # ハッシュアルゴリズム: SHA-256
+    # パスワード: user_create.password
+    # パスワードに追加するバイト: salt
+    # 繰り返し: 1000
+    hashed_password = hashlib.pbkdf2_hmac(
+        "sha256", user_create.password.encode(), salt, 1000
+    ).hex()
+
+    new_user = User(
+        username=user_create.username, password=hashed_password, salt=salt.decode()
+    )
     db.add(new_user)
     db.commit()
 
