162935d42150a1f50204408853c968fbc3b0f9ef
Kagami <shimura.kyohei7617@gmail.com>
2024-03-28 21:09:00 +0900

    feat: token schema

--- schemas.py
+++ schemas.py
@@ -47,3 +47,8 @@ class UserResponse(BaseModel):
     updated_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
